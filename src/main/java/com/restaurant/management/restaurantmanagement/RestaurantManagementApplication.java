46326c486e14652ee5fdd68d10706ec78301ca13
Bardia Namjoo <bardiademon@gmail.com>
2023-01-22 08:28:57 +0330

    First commit

---
+++ src/main/java/com/restaurant/management/restaurantmanagement/RestaurantManagementApplication.java
@@ -0,0 +1,15 @@
+package com.restaurant.management.restaurantmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RestaurantManagementApplication.class , args);
+    }
+
+}
---
+++ src/test/java/com/restaurant/management/restaurantmanagement/RestaurantManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package com.restaurant.management.restaurantmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
